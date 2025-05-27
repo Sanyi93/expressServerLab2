3c16fec530e5a242be33bbcd5fcda8b58ce091f3
TMS <TMSMSMSMSMSMS@bubububub.hu>
2025-05-27 15:49:43 -0400

    fetchMonth endpoint added

--- CD220Labs/expressjs/expressServer.js
+++ CD220Labs/expressjs/expressServer.js
@@ -6,6 +6,13 @@
 
  curl localhost:3333
 
+ trying endpoints
+curl -X POST http://localhost:3333/login/Jason
+curl http://localhost:3333/Jason
+curl http://localhost:3333/loginDetails
+
+Stopping the server by: "strg+ c"
+
  */
 
  // Import the Express.js library
@@ -36,6 +43,24 @@ app.post("/login/:name", (req, res) => {
 // Define a dynamic route to greet users by name
 app.get("/:name", (req, res) => {
     res.send("Hello " + req.params.name);
+});
+
+//defining an endpoint to fetch a particular month based on a number send in a request
+//array of months
+//run: curl http://localhost:3333/fetchMonth/7
+const months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+
+app.get("/fetchMonth/:num", (req, res) => {
+    //parsing number from the request
+    let num = parseInt(req.params.num);
+
+    if(num < 1 || num > 12){
+        //in case of invalid number
+        res.send("You have entered an invalid number");
+    } else {
+        //months[num-1] since array starts at 0
+        res.send("The month you requested is " + months[num-1]);
+    }
 });
 
 // Start the server and listen on port 3333
